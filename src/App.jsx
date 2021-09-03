383bf6e4348987a406d2b7873ea2e1c58e1c151d
Alex MacArthur <alex.macarthur@ramseysolutions.com>
2021-09-03 16:25:55 -0500

    stubbing out example project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState([]); 
+  const [newItemName, setNewItemName] = useState("");
+  const [newItemPrice, setNewItemPrice] = useState(0);
+
+  const calculateTotal = () => {
+    return items.reduce((total, item) => {
+      total = item.price + total;
+      return total;
+    }, 0);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  
+    setItems([...items, {
+      name: newItemName,
+      price: newItemPrice 
+    }])
+  }
+
   return (
     <div className="App">
-      Here's our app.
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name
+          <input type="text" onChange={(e) => setNewItemName(e.target.value)} /> 
+        </label>
+        
+        <label>
+          Price
+          <input type="text" onChange={(e) => setNewItemPrice(e.target.value)} />
+        </label>
+
+        <button>Add Item</button>
+      </form>
+      
+      <ul>
+        {items.map(({ name, price }) => {
+          return (
+            <li>
+              <span>
+                {name}
+              </span>
+
+              <span>
+                {price}
+              </span>
+            </li>
+          )
+        })}
+      </ul>
+
+      Total: {calculateTotal()}
     </div>
   )
 }
